f7ee786a90a151d2082983288bf84dddb9f48d3a
Roy Njeru <roynjeru24@gmail.com>
2025-10-02 11:40:23 -0400

    init

---
+++ src/OAuthJsonContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OAuthServer;
+
+[JsonSerializable(typeof(OAuthServerMetadata))]
+[JsonSerializable(typeof(AuthorizationServerMetadata))]
+[JsonSerializable(typeof(TokenResponse))]
+[JsonSerializable(typeof(JsonWebKeySet))]
+[JsonSerializable(typeof(JsonWebKey))]
+[JsonSerializable(typeof(TokenIntrospectionResponse))]
+[JsonSerializable(typeof(OAuthErrorResponse))]
+[JsonSerializable(typeof(ClientRegistrationRequest))]
+[JsonSerializable(typeof(ClientRegistrationResponse))]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+internal sealed partial class OAuthJsonContext : JsonSerializerContext;
